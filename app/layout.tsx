6ddbb9eb39340ae9b0a6137a8d112fa75531b891
mtcnbzks <mtcnbzks@gmail.com>
2024-05-31 21:14:02 +0300

    chore: Update title in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mehmetcan Bozkus",
+  title: "Mehmetcan",
   description: "Portfolio of Mehmetcan Bozkus",
 };
 
