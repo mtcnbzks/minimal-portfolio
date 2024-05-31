820967dc49a15d10364961d9c64eb913c2a8613a
mtcnbzks <mtcnbzks@gmail.com>
2024-05-31 20:13:59 +0300

    chore: Update metadata in layout.tsx and add social media links in constants/index.ts

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const socialMediaLinks = [
+  {
+    url: "https://www.linkedin.com/in/mtcnbzks",
+    imageSrc: "/images/linkedin-logo.png",
+    alt: "linkedin-profile",
+  },
+  {
+    url: "https://www.github.com/mtcnbzks",
+    imageSrc: "/images/github-logo.png",
+    alt: "github-profile",
+  },
+  {
+    url: "mailto:mtcnbzks@gmail.com",
+    imageSrc: "/images/gmail-logo.png",
+    alt: "email",
+  },
+];
